2b39b4b602043fa702e629f4d9dc88572af1dde7
MichaelDim02 <31562759+MichaelDim02@users.noreply.github.com>
2020-10-30 13:32:46 +0000

    Add files via upload

---
+++ bat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BATTERY=0
+BATTERY_INFO=$(acpi -b | grep "Battery ${BATTERY}")
+BATTERY_STATE=$(echo "${BATTERY_INFO}" | grep -wo "Full\|Charging\|Discharging")
+BATTERY_POWER=$(echo "${BATTERY_INFO}" | grep -o '[0-9]\+%' | tr -d '%')
+#BATTERY_INFO=${BATTERY_INFO:11}
+#ARRAY=(${BATTERY_INFO//,/ })
+#BATTERY_TIME="${ARRAY[2]} ${ARRAY[3]}"
+
+if [[ "${BATTERY_STATE}" = "Discharging" ]]; then
+	if [[ "${BATTERY_POWER}" -le 10 ]]; then		
+		# BATTERY < 10
+		notify-send -u critical "Low Battery" "Battery lower than 10"	
+		mpv siren.wav;
+	elif [[ "${BATTERY_POWER}" -le 20 ]]; then
+		# 10 < BATTERY < 20
+		notify-send "Low Battery" "Battery lower than 20"
+		mpv siren.wav;
+	fi
+fi
